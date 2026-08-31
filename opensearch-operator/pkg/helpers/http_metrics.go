c2dad1d92c06ab68b93e656da27138cd2da66ad4
Ram Singh Panwar <ram.panwar@tmdc.io>
2026-08-31 16:01:50 +0530

    Enabled metrics

--- opensearch-operator/pkg/helpers/http_metrics.go
+++ opensearch-operator/pkg/helpers/http_metrics.go
@@ -19,6 +19,9 @@ var (
 		prometheus.CounterOpts{
 			Name: "http_requests_total",
 			Help: "Total number of HTTP requests.",
+			ConstLabels: prometheus.Labels{
+				"app": "opensearch-operator",
+			},
 		},
 		[]string{"component", "method", "path"},
 	)
@@ -27,6 +30,9 @@ var (
 			Name:    "http_request_duration_seconds",
 			Help:    "HTTP request duration in seconds.",
 			Buckets: []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
+			ConstLabels: prometheus.Labels{
+				"app": "opensearch-operator",
+			},
 		},
 		[]string{"component", "method", "path"},
 	)
@@ -34,6 +40,9 @@ var (
 		prometheus.CounterOpts{
 			Name: "http_response_status_total",
 			Help: "Total number of HTTP responses by status code.",
+			ConstLabels: prometheus.Labels{
+				"app": "opensearch-operator",
+			},
 		},
 		[]string{"component", "method", "path", "status"},
 	)
